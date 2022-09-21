8015434292f8f37a760e6b7bbb8b46bc39c143ec
starydv7 <pawanstar72926@gmail.comm>
2022-09-21 13:11:41 +0530

    react-redux

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+      <div style={{
+          padding: "20px",
+          display: "flex",
+          gap:"30px"
+          
+    }}>
+      <Link to="/">Counter App</Link>
+      <Link to="/todos">Todos App</Link>
+    </div>
+  );
+}
+
+export default Navbar
